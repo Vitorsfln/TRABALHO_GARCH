7aec1ff052d875efe1cf7d0f29a6beddcc95bd3b
Vitor S <vitorsfln@hotmail.com>
2021-08-29 00:04:17 -0300

    Gráficos dos retornos + retirei acentos e caracteres especiais pois o github não gostou muito deles

--- trabalho_garch.R
+++ trabalho_garch.R
@@ -1,11 +1,11 @@
 
-#seleciona o diretório do arquivo como diretório de trabalho
+#seleciona o diretorio do arquivo como diretorio de trabalho
 my_d <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(my_d)
 
-#limpa as variáveis (shift+F10 se quiser resetar tudo incluindo plots, pacotes, console, etc)
+#limpa as variaveis (shift+F10 se quiser resetar tudo incluindo plots, pacotes, console, etc)
 rm(list=ls()) 
-#retira notação científica
+#retira notacao cientifica
 options(scipen = 999)
 
 
@@ -32,7 +32,7 @@ price.data <- df %>%
             period = 'daily',
             col_rename = 'ret')
 
-#retira as observações perdidas no cálculo dos retornos
+#retira as observacoes perdidas no calculo dos retornos
 price.data <- price.data[-c(1, 1235, 2469), ]
 
 port.data <- price.data %>% 
@@ -41,3 +41,57 @@ port.data <- price.data %>%
                weights = c(0.5, 0.3, 0.2),
                geometric = FALSE,
                col_rename = 'port.ret')
+
+
+
+#GRAFICOS DOS RETORNOS
+
+library(cowplot)
+
+p1 <- ggplot(price.data[1:1233, ], aes(data, ret)) + 
+  geom_line() + 
+  labs(title = ("BOVA11"),
+       x = " ",
+       y = "Retornos") + 
+  theme_bw() +
+  scale_y_continuous(labels = scales::percent) +
+  theme(panel.grid.minor = element_blank()) + 
+  geom_hline(yintercept = 0, color = 'red')
+
+p2 <- ggplot(price.data[1234:2466, ], aes(data, ret)) + 
+  geom_line() + 
+  labs(title = ("IVVB11"),
+       x = " ",
+       y = "Retornos") + 
+  theme_bw() +
+  scale_y_continuous(labels = scales::percent) +
+  theme(panel.grid.minor = element_blank()) + 
+  geom_hline(yintercept = 0, color = 'red')
+
+p3 <- ggplot(price.data[2467:3699, ], aes(data, ret)) + 
+  geom_line() + 
+  labs(title = ("SMAL11"),
+       x = " ",
+       y = "Retornos") + 
+  theme_bw() +
+  scale_y_continuous(labels = scales::percent) +
+  theme(panel.grid.minor = element_blank()) + 
+  geom_hline(yintercept = 0, color = 'red') 
+
+p4 <- ggplot(port.data, aes(data, port.ret)) + 
+  geom_line() + 
+  labs(title = ("PORTFOLIO"),
+       x = " ",
+       y = "Retornos") + 
+  theme_bw() +
+  scale_y_continuous(labels = scales::percent) +
+  theme(panel.grid.minor = element_blank()) + 
+  geom_hline(yintercept = 0, color = 'red') 
+
+grid1 <- plot_grid(p1, p2, p3, p4, nrow = 2, 
+                   labels = " ")
+grid1
+
+
+#ESTAT?STICAS DESCRITIVAS
+
